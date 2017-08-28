a20c1f0a4bc18772c6b0316effcaf32b6177b88b
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2017-08-28 11:28:57 +0700

    Implement new filtering language parsing
    * basic_filters.c (lookup_class, qualify_syscall_class, qualify_syscall,
    qualify_syscall_tokens, parse_syscall_filter, qualify_tokens,
    parse_fd_filter, parse_path_filter): Add qualify_mode argument.
    (qualify_tokens, qualify_syscall_tokens): Use set inversion only in
    qualify mode.
    (lookup_class): Use deprecated class names only in qualify mode.
    * defs.h (qualify): Remove declaration.
    (filtering_parse): Add new declaration.
    * filter.c (struct filter_type, parse_filter):
    Add bool argument to parse_*_filter declarations.
    * filter.h (parse_filter_action, parse_qualify_action,
    parse_filter_expression): Add new declarations.
    (parse_filter, qualify_tokens, qualify_syscall_tokens):
    Add qualify_mode argument.
    (DECL_FILTER): Add bool argument to parse_*_filter declarations.
    * filter_action.c (parse_filter_action): Add new parsing function.
    (inject_path_tracing): Use filtering_parse instead of qualify.
    * filter_expression.c (parse_filter_expression): Implement parsing of filter
    expression.
    (parse_operator, push_operator, is_higher_priority): Add helper functions.
    (is_space_ascii, is_allowed_in_name): Add new declarations.
    * filter_parse.c: New file.
    * filter_qualify.c (qualify_read, qualify_write, qualify_signals,
    qualify_trace, qualify_abbrev, qualify_verbose, qualify_raw,
    qualify_inject_common, qualify_fault, qualify_inject): Use main_part and args
    arguments.
    * strace.c (init): Use filtering_parse instead of qualify.
    * Makefile.am (strace_SOURCES): Add filter_parse.c.
    
    [ldv: simplify *_qualify_mode]
    [ldv: eliminate parse_null]
    [ldv: optimize lookup_class]
    [ldv: use loop initial declarations]
    [ldv: ATTRIBUTE_FALLTHROUGH]

--- strace.c
+++ strace.c
@@ -1573,7 +1573,7 @@ init(int argc, char *argv[])
 #if DEFAULT_QUAL_FLAGS != (QUAL_TRACE | QUAL_ABBREV | QUAL_VERBOSE)
 # error Bug in DEFAULT_QUAL_FLAGS
 #endif
-	qualify("signal=all");
+	filtering_parse("signal=all");
 	while ((c = getopt(argc, argv, "+"
 #ifdef USE_LIBUNWIND
 	    "k"
@@ -1610,7 +1610,7 @@ init(int argc, char *argv[])
 			daemonized_tracer = 1;
 			break;
 		case 'e':
-			qualify(optarg);
+			filtering_parse(optarg);
 			break;
 		case 'E':
 			if (putenv(optarg) < 0)
@@ -1678,7 +1678,7 @@ init(int argc, char *argv[])
 			username = optarg;
 			break;
 		case 'v':
-			qualify("abbrev=none");
+			filtering_parse("abbrev=none");
 			break;
 		case 'V':
 			print_version();
